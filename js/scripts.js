d1efeeb72a042c3b1cd88b0db36e98c3e0745518
igl-myrick <igl.myrick@gmail.com>
2024-01-25 14:27:08 -0800

    add exceptions for numbers containing 1, 2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 // business logic
 
 function beepBoop(inputNum) {
-  let neighborhoodArr = []
+  let neighborhoodArr = [];
   for (let i = 0; i < inputNum; i+=1) {
     neighborhoodArr.push(i);
   }
   neighborhoodArr.push(inputNum);
-  console.log(neighborhoodArr);
+  neighborhoodArr.forEach(function(number, index) {
+    if (number.toString().includes(3)) {
+      neighborhoodArr.splice(index, 1, "Will you be my neighbor?");
+    } else if (number.toString().includes(2)) {
+      neighborhoodArr.splice(index, 1, "Boop!");
+    } else if (number.toString().includes(1)) {
+      neighborhoodArr.splice(index, 1, "Beep!");
+    }
+  });
+  return neighborhoodArr;
 }
-
-beepBoop(8);
